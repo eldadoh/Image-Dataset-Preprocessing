082be60443d6dbdeeffdfb479be7a90a88cc3988
eldad <eldad@arpalus.com>
2021-07-12 16:03:49 +0300

    .

--- handle_single_csv_dir.py
+++ handle_single_csv_dir.py
@@ -15,22 +15,27 @@ def sort_annotations_df_by_class(df : pd.DataFrame,dir_path : str) -> pd.DataFra
     
     return df 
 
+def get_specific_class_samples(df : pd.DataFrame,class_name:str) -> pd.DataFrame:
+
+    df = df[df['annotation'] == class_name]
+    
+    if df.empty:
+        print(f'Could not get ==={class_name}=== samples from current dataframe')
+    
+    return df 
+
 def handle_single_csv_dir_func(dir_path : str,annotation_file_name_const : str, verbose : bool = True) -> pd.DataFrame:
 
     annotation_file_path = os.path.join(dir_path,annotation_file_name_const)
     
-    basic_raw_df = pd.read_csv(annotation_file_path)
-
-    try : 
-        df_by_class = sort_annotations_df_by_class(basic_raw_df,dir_path)
-        print
-    except Exception as e : 
-        print(e)
+    df = pd.read_csv(annotation_file_path)
+    df_sorted_class = sort_annotations_df_by_class(df,dir_path)
+    specific_class_df_samples = get_specific_class_samples(df_sorted_class,class_name='9702910116_THERABREATH ORAL RINSE MINT')
 
     if verbose : 
         print ('\nDone with single_dir_csv ===> ' + f'{os.path.dirname(annotation_file_path)}')
 
-    return df_by_class 
+    return specific_class_df_samples 
 
 if __name__ == '__main__':
 
@@ -41,5 +46,6 @@ if __name__ == '__main__':
     df_name = 'Data/test.pkl'
 
     df = handle_single_csv_dir_func(test_path_single_csv_dir,ANNOTATION_FILE_NAME_CONST)
-
+    
+    df.to_csv('_.csv', sep = '\t' , float_format='{:5f}'.format, encoding='utf-8') 
     pass 
--- pandas_utils.py
+++ pandas_utils.py
@@ -11,4 +11,33 @@ def load_df(name : str) -> pd.DataFrame :
     
     return pd.read_pickle(name)
     
+
+def save_df_as_csv(df : pd.DataFrame,name:str):
+
+    """float_format='{:5f}'"""
     
+    df.to_csv(f'{name}', sep = '\t' , float_format='{:5f}'.format, encoding='utf-8')
+
+def xl_to_pandas(file_name,sheet_name = None):
+    
+    xl_file = pd.ExcelFile(file_name)
+
+    dfs = {sheet_name: xl_file.parse(sheet_name) 
+          for sheet_name in xl_file.sheet_names}
+
+    if sheet_name:
+          
+          return dfs[f'{sheet_name}']
+
+    return dfs 
+
+# def remove_scientific_notation(df,col_name, new_dtype = 'int32'):
+
+#       if not isinstance(col_name,str):
+#             print('col_name is not a string')
+#             return False
+
+#       df[col_name] =  df[col_name].astype(new_dtype)
+#           df['SKU'] = df['SKU'].astype(np.int64)
+#     pd.DataFrame.to_csv(df,'_.csv', sep = '\t' , float_format='{:f}'.format, encoding='utf-8') 
+#       return df 
