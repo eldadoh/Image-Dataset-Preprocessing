1c5407e9d24ea327d1b29b35ccee88ac8c37c539
eldad <eldad@arpalus.com>
2021-07-13 22:11:38 +0300

    df for all class specific was done

--- clear_annotations_analysis.py
+++ clear_annotations_analysis.py
@@ -11,27 +11,47 @@ from pandas_utils import save_df_as_csv
 
 """ In this file I am running some analysis functions on the clear aggeregated annotations files data"""
 
-def get_specific_class_samples(df_as_csv ,class_name: str,save: bool = False):
+def get_specific_class_samples(df_as_csv ,class_name: str,output_dir_path :str , save: bool = False , class_names_who_changed : list = None):
 
     df = pd.read_csv(df_as_csv,sep='\t')
 
     specific_class_df = df[df['annotation'] == class_name]
     
     if specific_class_df.empty:
-        print(f'Could not get ==={class_name}=== samples from current dataframe')
+        print(f'Could not get ==={class_name}=== samples from current dataframe because it\'s empty')
     
     if save : 
-        save_df_as_csv(specific_class_df,'Output/specific_class_df_samples.csv')
-        print(f'\nSaved df samples from class === {class_name} === to : Output/specific_class_df_samples.csv\n')
+        output_path = os.path.join(output_dir_path , f'{class_name}.csv')
+        try:
+            save_df_as_csv(specific_class_df,output_path)
+            print(f'\nSaved df samples from class === {class_name} === to : {output_path}\n')
+        except:
+            print(f'\nBad class name: {class_name}')
+            class_name = class_name.replace("/", "@" , -1)
+            output_path = os.path.join(output_dir_path , f'{class_name}.csv')
+            save_df_as_csv(specific_class_df,output_path)
+            print(f'\nChanged and Saved df samples from class === {class_name} === to : {output_path}\n')
+            class_names_who_changed.append(class_name)
+    
+    return specific_class_df,class_names_who_changed
 
-    return specific_class_df
+def get_all_dataset_specific_class_anotations(df_as_csv : pd.DataFrame,output_path :str , save : bool = False,class_names_who_changed : list = [],verbose :bool = True ):
+
+    df = pd.read_csv(df_as_csv,sep='\t')
+    class_names_list = df['annotation'].unique()
+
+    for class_name in class_names_list : 
+        _ , class_names_who_changed = get_specific_class_samples(df_as_csv,class_name,output_path,save,class_names_who_changed)
+
+    if verbose : 
+        print(f'The next classes names had changed:\n{class_names_who_changed}\n')
 
 def main():     
 
     DF_AS_CSV_FILE_PATH = 'Output/clear_df.csv'
-    CLASS_NAME = '9702910116_THERABREATH ORAL RINSE MINT'
+    ANNOTATIONS_BY_CLASS_DIR_PATH = 'Output/annotations_by_class'
 
-    get_specific_class_samples(DF_AS_CSV_FILE_PATH ,class_name = CLASS_NAME ,save = True)
-        
+    get_all_dataset_specific_class_anotations(DF_AS_CSV_FILE_PATH,output_path=ANNOTATIONS_BY_CLASS_DIR_PATH,save = True)
+
 if __name__ == '__main__':
     main()
